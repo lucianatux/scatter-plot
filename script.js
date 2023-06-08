94c238612a964af070154a51b98eddfe157fbf43
lucianatux <lucianatux@yahoo.com.ar>
2023-06-08 20:37:59 -0300

    variables en ingles

--- script.js
+++ script.js
@@ -3,63 +3,63 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
   .then(response => response.json())
   .then(data => {
     // Constantes para el gráfico
-    const margen = { superior: 40, derecho: 20, inferior: 60, izquierdo: 60 };
-    const ancho = 800 - margen.izquierdo - margen.derecho;
-    const alto = 400 - margen.superior - margen.inferior;
+    const margin = { top: 40, right: 20, bottom: 60, left: 60 };
+    const width = 800 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-    // Analizar la fecha y hora
+    // Parsear la fecha y hora
     const parseTime = d3.timeParse('%M:%S');
     data.forEach(d => {
-      const tiempoParseado = parseTime(d.Time);
-      d.Time = new Date(1970, 0, 1, 0, tiempoParseado.getMinutes(), tiempoParseado.getSeconds());
+      const parsedTime = parseTime(d.Time);
+      d.Time = new Date(1970, 0, 1, 0, parsedTime.getMinutes(), parsedTime.getSeconds());
     });
 
     // Crear escalas para los ejes x e y
-    const escalaX = d3.scaleLinear()
+    const xScale = d3.scaleLinear()
       .domain([d3.min(data, d => d.Year - 1), d3.max(data, d => d.Year + 1)])
-      .range([0, ancho]);
+      .range([0, width]);
 
-    const escalaY = d3.scaleTime()
+    const yScale = d3.scaleTime()
       .domain(d3.extent(data, d => d.Time))
-      .range([alto, 0]);
+      .range([height, 0]);
 
-    // Crear los ejes x e y
-    const ejeX = d3.axisBottom(escalaX).tickFormat(d3.format('d'));
-    const ejeY = d3.axisLeft(escalaY).tickFormat(d3.timeFormat('%M:%S'));
+    // Crear ejes x e y
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat('%M:%S'));
 
     // Crear el gráfico de dispersión
     const svg = d3.select('#scatterplot')
-      .attr('width', ancho + margen.izquierdo + margen.derecho)
-      .attr('height', alto + margen.superior + margen.inferior)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
       .append('g')
-      .attr('transform', `translate(${margen.izquierdo}, ${margen.superior})`);
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    // Agregar el eje x
+    // Agregar eje x
     svg.append('g')
       .attr('id', 'x-axis')
-      .attr('transform', `translate(0, ${alto})`)
-      .call(ejeX);
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis);
 
-    // Agregar el eje y
+    // Agregar eje y
     svg.append('g')
       .attr('id', 'y-axis')
-      .call(ejeY);
+      .call(yAxis);
 
-    // Agregar los puntos
+    // Agregar puntos
     svg.selectAll('.dot')
       .data(data)
       .enter()
       .append('circle')
       .attr('class', 'dot')
-      .attr('cx', d => escalaX(d.Year))
-      .attr('cy', d => escalaY(d.Time))
+      .attr('cx', d => xScale(d.Year))
+      .attr('cy', d => yScale(d.Time))
       .attr('r', 5)
       .attr('data-xvalue', d => d.Year)
       .attr('data-yvalue', d => d.Time.toISOString())
       .on('mouseover', (event, d) => {
         const tooltip = d3.select('#tooltip');
         tooltip.attr('data-year', d.Year)
-          .html(`${d.Name}: ${d.Nationality}<br>Año: ${d.Year}, Tiempo: ${d.Time.getMinutes()}:${d.Time.getSeconds()}`)
+          .html(`${d.Name}: ${d.Nationality}<br>Year: ${d.Year}, Time: ${d.Time.getMinutes()}:${d.Time.getSeconds()}`)
           .style('left', `${event.pageX + 10}px`)
           .style('top', `${event.pageY}px`)
           .transition()
@@ -73,19 +73,19 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
           .style('opacity', 0);
       });
 
-    // Agregar la leyenda
-    const leyenda = svg.append('g')
+    // Agregar leyenda
+    const legend = svg.append('g')
       .attr('id', 'legend')
-      .attr('transform', `translate(${ancho - 180}, ${alto - 20})`);
+      .attr('transform', `translate(${width - 180}, ${height - 20})`);
 
-    leyenda.append('rect')
+    legend.append('rect')
       .attr('width', 15)
       .attr('height', 15)
       .style('fill', '#cb34d0');
 
-    leyenda.append('text')
+    legend.append('text')
       .attr('x', 15)
       .attr('y', 10)
-      .text('Sin acusaciones de dopaje');
+      .text('No doping allegations');
   });
 
